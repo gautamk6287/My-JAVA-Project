7032359a1255689cd427c748f620bd378b814937
Gautam kumar <gautam.kumar628700@gmail.com>
2025-08-06 10:20:05 +0530

    Add files via upload

---
+++ My Java Prog/Hello_Constructor.java
@@ -0,0 +1,14 @@
+package com.gautam;
+
+public class Hello_Constructor {
+
+    
+    Hello_Constructor() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+       
+        new Hello_Constructor();
+    }
+}
